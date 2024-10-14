6b71f2c554b91097c58b81830b09dcd3960dcd9d
hualde <javhualde@gmail.com>
2024-10-14 16:19:26 +0200

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import React from 'react'
+import { AppProps } from 'next/app'
+import { Analytics } from "@vercel/analytics/react"
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp
